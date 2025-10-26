e2a54de455d6fa0041009cd137e8c5bf549d7512
Anuj pundora <anuj28279@gmail.com>
2025-10-26 22:18:00 +0530

    Initial commit

---
+++ AP/src/test/java/AnujPundora/AP/ApApplicationTests.java
@@ -0,0 +1,13 @@
+package AnujPundora.AP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ AnujPundora/src/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <div className=" h-fullscreen w-100 bg-black">
+        <h1>Anuj pundora</h1>
+        </div>
+    )
+}
