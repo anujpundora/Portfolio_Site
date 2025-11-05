7181eab0f3e714b7791eda9e7dcf944891fb6e82
Anuj pundora <anuj28279@gmail.com>
2025-11-05 23:45:38 +0530

    Added external func2

--- AnujPundora/src/Components/HeroSection/Home.jsx
+++ AnujPundora/src/Components/HeroSection/Home.jsx
@@ -1,12 +1,11 @@
-import HomeAnimation from "../Animations/HomeAnimation";
-import Typewriter from 'typewriter-effect';
+import HomeAnimation from "./Animations/HomeAnimation";
 function Home(){
     return(
         <div id="home" className="flex  items-center justify-around pt-0  pr-0 p-15 pb-0 ">
             <div id="hm-txt" className="flex-col justify-center space-y-20 items-center max-w-1/3 text-[#FFB606] font-extrabold text-5xl "> 
                     <h6 className="text-5xl ">Launching ideas into the digital cosmos</h6>
                   
-                    <span className=" font-bold  bg-linear-to-r from-[#FFB606] to-[#cba673] bg-clip-text text-transparent">
+                    <span className=" font-bold  bg-linear-to-r from-[#FFB606] to-[#cba673]  text-transparent">
                         I ' m Anuj Pundora</span>  
                     <div className=" text-xl mt-3  font-thin">                   
                     <Typewriter
